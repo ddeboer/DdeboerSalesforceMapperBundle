406eaa1291fd66450572ba02a27720d97837a571
David de Boer <david@ddeboer.nl>
2011-12-28 16:26:35 +0100

    Update mapper interface to include match field

--- MappedBulkSaver.php
+++ MappedBulkSaver.php
@@ -38,16 +38,7 @@ class MappedBulkSaver implements MappedBulkSaverInterface
     }
 
     /**
-     * Add a model to the bulk save queue
-     *
-     * Please note that saving this model to Salesforce is delayed until the
-     * queue is full. 
-     *
-     * @param mixed $model        Any properly annotation object is allowed. It
-     *                            is possible to save Salesforce records of
-     *                            different object types.
-     * @param string $matchField  Optional field to match by, for upserts
-     * @return MappedBulkSaver
+     * {@inheritdoc}
      */
     public function save($model, $matchField = null)
     {
@@ -59,15 +50,7 @@ class MappedBulkSaver implements MappedBulkSaverInterface
     }
 
     /**
-     * Add a model to the delete queue
-     *
-     * Please note that deleting this model from Salesforce is delayed until the
-     * queue is full. 
-     * 
-     * @param object $model     Any object that has a getId() method is allowed.
-     *                          It is possible to delete Salesforce records of
-     *                          different object types.
-     * @return MappedBulkSaver
+     * {@inheritdoc}
      */
     public function delete($model)
     {
@@ -79,9 +62,7 @@ class MappedBulkSaver implements MappedBulkSaverInterface
     }
 
     /**
-     * Issue all queued creates, deletes, updates and upserts to Salesforce
-     *
-     * @return SaveResult[]
+     * {@inheritdoc}
      */
     public function flush()
     {
--- MappedBulkSaverInterface.php
+++ MappedBulkSaverInterface.php
@@ -2,9 +2,41 @@
 
 namespace Ddeboer\Salesforce\MapperBundle;
 
+use Ddeboer\Salesforce\ClientBundle\Response\SaveResult;
+
 interface MappedBulkSaverInterface
 {
-    function save($model);
+    /**
+     * Add a model to the bulk save queue
+     *
+     * Please note that saving this model to Salesforce is delayed until the
+     * queue is full. 
+     *
+     * @param object $model       Any properly annotation object is allowed. It
+     *                            is possible to save Salesforce records of
+     *                            different object types.
+     * @param string $matchField  Optional field to match by, for upserts
+     * @return MappedBulkSaverInterface
+     */
+    function save($model, $matchField = null);
+
+    /**
+     * Add a model to the delete queue
+     *
+     * Please note that deleting this model from Salesforce is delayed until the
+     * queue is full.
+     *
+     * @param object $model     Any object that has a getId() method is allowed.
+     *                          It is possible to delete Salesforce records of
+     *                          different object types.
+     * @return MappedBulkSaverInterface
+     */
     function delete($model);
+
+    /**
+     * Issue all queued creates, deletes, updates and upserts to Salesforce
+     *
+     * @return SaveResult[]
+     */
     function flush();
 }
