ddeboer/DdeboerSalesforceMapperBundle

commit 4dfda95ec3f95b617d5ab7befdf036dbedf6b395

Cache mapped objects in record iterator, so save($mappedRecordIterator) works correctly

Response/MappedRecordIterator.php
@@ -35,6 +35,13 @@ class MappedRecordIterator implements \OuterIterator, \Countable
     protected $modelClass;
 
     /**
+     * Mapped cache
+     *
+     * @var array
+     */
+    protected $mappedCache = array();
+
+    /**
      * Construct a mapped record iterator
      *
      * @param RecordIterator $recordIterator
@@ -67,8 +74,14 @@ class MappedRecordIterator implements \OuterIterator, \Countable
      */
     public function current()
     {
-        return $this->mapper->mapToDomainObject(
-            $this->recordIterator->current(), $this->modelClass);
+        if (!isset($this->mappedCache[$this->key()])) {
+            $this->mappedCache[$this->key()] = $this->mapper->mapToDomainObject(
+                $this->recordIterator->current(),
+                $this->modelClass
+            );
+        }
+
+        return $this->mappedCache[$this->key()];
     }
 
     /**
@@ -110,11 +123,9 @@ class MappedRecordIterator implements \OuterIterator, \Countable
      */
     public function first()
     {
-        if ($this->recordIterator->first()) {
-            return $this->mapper->mapToDomainObject(
-                $this->recordIterator->first(), $this->modelClass
-            );
-        }
+        $this->rewind();
+        
+        return $this->current();
     }
 
     /**
@@ -129,7 +140,7 @@ class MappedRecordIterator implements \OuterIterator, \Countable
 
     /**
      * Get object at key
-     * 
+     *
      * @param int $key
      * @return object | null
      */

commit bcbb2cc319d2ff8dc672816bc8ff77013cd69323

Fix bug introduced with 4dfda95ec3f95b617d5ab7befdf036dbedf6b395

Response/MappedRecordIterator.php
@@ -74,14 +74,7 @@ class MappedRecordIterator implements \OuterIterator, \Countable
      */
     public function current()
     {
-        if (!isset($this->mappedCache[$this->key()])) {
-            $this->mappedCache[$this->key()] = $this->mapper->mapToDomainObject(
-                $this->recordIterator->current(),
-                $this->modelClass
-            );
-        }
-
-        return $this->mappedCache[$this->key()];
+        return $this->get($this->key());
     }
 
     /**
@@ -123,9 +116,7 @@ class MappedRecordIterator implements \OuterIterator, \Countable
      */
     public function first()
     {
-        $this->rewind();
-        
-        return $this->current();
+        return $this->get(0);
     }
 
     /**
@@ -146,9 +137,15 @@ class MappedRecordIterator implements \OuterIterator, \Countable
      */
     public function get($key)
     {
-        $sObject = $this->recordIterator->seek($key);
-        if ($sObject) {
-            return $this->mapper->mapToDomainObject($sObject, $this->modelClass);
+        if (!isset($this->mappedCache[$this->key()])) {
+            $sObject = $this->recordIterator->seek($key);
+            if (!$sObject) {
+                throw \OutOfBoundsException();
+            }
+
+            $this->mappedCache[$key] = $this->mapper->mapToDomainObject($sObject, $this->modelClass);
         }
+
+        return $this->mappedCache[$key];
     }
 }