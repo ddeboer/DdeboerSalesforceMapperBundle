0188ad4715896c8cfc00a817cd700bae1113c1ca
asim-byng <asim.liaquat@byng-systems.com>
2014-02-13 18:19:35 +0000

    Case:FE-151: interim commit, im still working on this

---
+++ Model/SalesforceCase.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Ddeboer\Salesforce\MapperBundle\Model;
+
+use Ddeboer\Salesforce\MapperBundle\Annotation as Salesforce;
+
+/**
+ * Salesforce standard task object
+ *
+ * @Salesforce\Object(name="Case")
+ */
+class SalesforceCase extends AbstractModel
+{
+    /**
+     * @var Account
+     * @Salesforce\Relation(field="AccountId", name="Account",
+     *                      class="Ddeboer\Salesforce\MapperBundle\Model\Account")
+     */
+    protected $account;
+
+    /**
+     * @var string
+     * @Salesforce\Field(name="AccountId")
+     */
+    protected $accountId;
+    
+    /**
+     * @var string
+     * @Salesforce\Field(name="Description")
+     */
+    protected $description;
+    
+    /**
+     * @var User
+     * @Salesforce\Relation(field="OwnerId", name="Owner",
+     *                      class="Ddeboer\Salesforce\MapperBundle\Model\User")
+     */
+    protected $owner;
+
+    /**
+     * @var string
+     * @Salesforce\Field(name="OwnerId")
+     */
+    protected $ownerId;
+    
+    public function getAccount() {
+        return $this->account;
+    }
+
+    public function setAccount($account) {
+        $this->account = $account;
+    }
+
+    public function getAccountId() {
+        return $this->accountId;
+    }
+
+    public function setAccountId($accountId) {
+        $this->accountId = $accountId;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+
+    public function getOwner() {
+        return $this->owner;
+    }
+
+    public function setOwner($owner) {
+        $this->owner = $owner;
+    }
+
+    public function getOwnerId() {
+        return $this->ownerId;
+    }
+
+    public function setOwnerId($ownerId) {
+        $this->ownerId = $ownerId;
+    }
+
+}
